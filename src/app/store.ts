d160d29f502770cdb569a0e75814ee4ecb2d5e5a
MaxOF <egorovteam2019@yandex.ru>
2022-04-12 21:13:49 +0300

    add redux-toolkit and create new store with configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,31 @@
 import {tasksReducer} from '../features/TodolistList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistList/todolists-reducer';
-import {applyMiddleware, combineReducers, createStore} from 'redux';
+import {combineReducers} from 'redux';
 import thunk from "redux-thunk";
 import {appReducer} from "./app-reducer";
 import {authReducer} from "../features/Login/auth-reducer";
+import {configureStore} from "@reduxjs/toolkit";
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
+
+
+
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
     app: appReducer,
     auth: authReducer,
 })
-// непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunk));
-// определить автоматически тип всего объекта состояния
+
+// export const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk)
+})
+
+
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// in order to apply to store in object window
 // @ts-ignore
 window.store = store;
