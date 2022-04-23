be39804beec76ad78293133648e14110bfaa93eb
MaxOF <egorovteam2019@yandex.ru>
2022-04-23 21:32:06 +0300

    refactor createAsyncThunk

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,31 +1,32 @@
-import { Dispatch } from 'redux'
-import { setAppStatusAC } from '../../app/app-reducer'
+import {Dispatch} from 'redux'
+import {setAppStatusAC} from '../../app/app-reducer'
 import {authAPI, LoginParamsType} from "../../api/todolists-api";
 import {handleServerAppError, handleServerNetwork} from "../../utils/error-utils";
 import {clearTodosDataAC} from "../TodolistList/todolists-reducer";
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 //initial state >>>>>>
 
-const initialState = {
-    isLoggedIn: false
-}
-const slice = createSlice({
-    name: 'auth',
-    initialState: initialState,
-    reducers: {
-        setIsLoggedInAC(state, action: PayloadAction<{value: boolean}>){
-            state.isLoggedIn = action.payload.value
+export const loginTC = createAsyncThunk('auth/login', async (param: LoginParamsType, thunkAPI) => {
+    thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
+
+    try {
+        const res = await authAPI.login(param)
+        if (res.data.resultCode === 0) {
+            thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
+            return {isLoggedIn: true}
+        } else {
+            handleServerAppError(res.data, thunkAPI.dispatch)
+            return {isLoggedIn: false}
         }
+    } catch (error: any) {
+        handleServerNetwork({message: error}, thunkAPI.dispatch)
+        return {isLoggedIn: false}
     }
+
 })
 
-export const authReducer = slice.reducer
-export const {setIsLoggedInAC} = slice.actions
-
-// thunks >>>>>>
-
-export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
+export const loginTC_ = (data: LoginParamsType) => (dispatch: Dispatch) => {
     dispatch(setAppStatusAC({status: 'loading'}))
     authAPI.login(data)
         .then(res => {
@@ -40,13 +41,37 @@ export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
             handleServerNetwork(error, dispatch)
         })
 }
+
+const slice = createSlice({
+    name: 'auth',
+    initialState: {
+        isLoggedIn: false
+    },
+    reducers: {
+        setIsLoggedInAC(state, action: PayloadAction<{ value: boolean }>) {
+            state.isLoggedIn = action.payload.value
+        }
+    },
+    extraReducers: builder => {
+        builder.addCase(loginTC.fulfilled, (state, action) => {
+            state.isLoggedIn = action.payload.isLoggedIn
+        })
+    }
+})
+
+export const authReducer = slice.reducer
+export const {setIsLoggedInAC} = slice.actions
+
+// thunks >>>>>>
+
+
 export const logoutTC = () => (dispatch: Dispatch) => {
     dispatch(setAppStatusAC({status: 'loading'}))
     authAPI.logout()
         .then(res => {
-            if(res.data.resultCode === 0) {
+            if (res.data.resultCode === 0) {
                 dispatch(setIsLoggedInAC({value: false}))
-                dispatch(setAppStatusAC({status: 'succeeded'}   ))
+                dispatch(setAppStatusAC({status: 'succeeded'}))
                 dispatch(clearTodosDataAC())
             } else {
                 handleServerAppError(res.data, dispatch)
--- src/features/TodolistList/tasks-reducer.ts
+++ src/features/TodolistList/tasks-reducer.ts
@@ -29,27 +29,22 @@ const initialState: TasksStateType = {}
 
 //thunks>>>>>>>>>>>>>>>
 
-export const fetchTasksTC = createAsyncThunk('tasks/fetchTasks', (todolistId: string, thunkAPI) => {
+export const fetchTasksTC = createAsyncThunk('tasks/fetchTasks', async (todolistId: string, thunkAPI) => {
     thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
-    return todolistsAPI.getTasks(todolistId)
-        .then(res => {
-            // thunkAPI.dispatch(setTasksAC({tasks: res.data.items, todolistId}))
-            const tasks = res.data.items
-            thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
-            return {tasks, todolistId}
-        })
+    const res = await todolistsAPI.getTasks(todolistId)
+    const tasks = res.data.items
+    thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
+    return {tasks, todolistId}
 })
 
-export const removeTaskTC = createAsyncThunk('tasks/removeTask', (param: {taskId: string, todolistId: string}, thunkAPI) => {
+export const removeTaskTC = createAsyncThunk('tasks/removeTask', async (param: { taskId: string, todolistId: string }, thunkAPI) => {
     thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
-    return todolistsAPI.deleteTask(param.todolistId, param.taskId)
-        .then(res => {
-            thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
-            return {taskId: param.taskId, todolistId: param.todolistId}
-        })
+    const res = await todolistsAPI.deleteTask(param.todolistId, param.taskId)
+    return {taskId: param.taskId, todolistId: param.todolistId}
+
 })
 
-export const createTaskTC = createAsyncThunk ('tasks/createTask', ((param: {todolistId: string, title: string}, thunkAPI) => {
+export const createTaskTC = createAsyncThunk('tasks/createTask', ((param: { todolistId: string, title: string }, thunkAPI) => {
     thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
     return todolistsAPI.createTask(param.todolistId, param.title)
         .then(res => {
@@ -68,36 +63,36 @@ export const createTaskTC = createAsyncThunk ('tasks/createTask', ((param: {todo
 
 export const updateTaskTC = (taskId: string, model: UpdateDomainTaskModelType, todolistId: string,) =>
     (dispatch: Dispatch, getState: () => AppRootStateType) => {
-            dispatch(setAppStatusAC({status: 'loading'}))
-            const state = getState()
-            const task = state.tasks[todolistId].find(t => t.id === taskId)
-            if (!task) {
-                //throw new Error("task not found in the state");
-                console.warn('task not found in the state')
-                return
-            }
-            const apiModel: UpdateTaskModelType = {
-                deadline: task.deadline,
-                description: task.description,
-                priority: task.priority,
-                startDate: task.startDate,
-                title: task.title,
-                status: task.status,
-                ...model
-            }
-            todolistsAPI.updateTask(todolistId, taskId, apiModel)
-                .then(res => {
-                    if (res.data.resultCode === 0) {
-                        dispatch(updateTaskAC({taskId, model, todolistId}))
-                        dispatch(setAppStatusAC({status: 'succeeded'}))
-                    } else {
-                        handleServerAppError(res.data, dispatch)
-                    }
-                })
-                .catch((error) => {
-                    handleServerNetwork(error, dispatch)
-                })
-}
+        dispatch(setAppStatusAC({status: 'loading'}))
+        const state = getState()
+        const task = state.tasks[todolistId].find(t => t.id === taskId)
+        if (!task) {
+            //throw new Error("task not found in the state");
+            console.warn('task not found in the state')
+            return
+        }
+        const apiModel: UpdateTaskModelType = {
+            deadline: task.deadline,
+            description: task.description,
+            priority: task.priority,
+            startDate: task.startDate,
+            title: task.title,
+            status: task.status,
+            ...model
+        }
+        todolistsAPI.updateTask(todolistId, taskId, apiModel)
+            .then(res => {
+                if (res.data.resultCode === 0) {
+                    dispatch(updateTaskAC({taskId, model, todolistId}))
+                    dispatch(setAppStatusAC({status: 'succeeded'}))
+                } else {
+                    handleServerAppError(res.data, dispatch)
+                }
+            })
+            .catch((error) => {
+                handleServerNetwork(error, dispatch)
+            })
+    }
 
 
 const slice = createSlice({
